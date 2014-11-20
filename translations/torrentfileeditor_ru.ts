2a1969ead0f495511083a2779e3d23568189143b
Ivan Romanov <drizt@land.ru>
2014-11-20 23:24:13 +0500

    update translations

--- translations/torrentfileeditor_ru.ts
+++ translations/torrentfileeditor_ru.ts
@@ -58,150 +58,156 @@
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="14"/>
-        <location filename="../linux/ui_mainwindow.h" line="543"/>
+        <location filename="../linux/ui_mainwindow.h" line="564"/>
         <source>MainWindow</source>
         <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="21"/>
-        <location filename="../linux/ui_mainwindow.h" line="544"/>
+        <location filename="../linux/ui_mainwindow.h" line="565"/>
         <source>New</source>
         <translation>Новый</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="28"/>
-        <location filename="../linux/ui_mainwindow.h" line="545"/>
-        <location filename="../mainwindow.cpp" line="207"/>
+        <location filename="../linux/ui_mainwindow.h" line="566"/>
+        <location filename="../mainwindow.cpp" line="210"/>
         <source>Open</source>
         <translation>Открыть</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="35"/>
-        <location filename="../linux/ui_mainwindow.h" line="546"/>
+        <location filename="../linux/ui_mainwindow.h" line="567"/>
         <source>Save</source>
         <translation>Сохранить</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="42"/>
-        <location filename="../linux/ui_mainwindow.h" line="547"/>
-        <location filename="../mainwindow.cpp" line="243"/>
+        <location filename="../linux/ui_mainwindow.h" line="568"/>
+        <location filename="../mainwindow.cpp" line="246"/>
         <source>Save As</source>
         <translation>Сохранить как</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="62"/>
-        <location filename="../linux/ui_mainwindow.h" line="548"/>
+        <location filename="../linux/ui_mainwindow.h" line="569"/>
         <source>About</source>
         <translation>О программе</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="73"/>
-        <location filename="../linux/ui_mainwindow.h" line="559"/>
+        <location filename="../linux/ui_mainwindow.h" line="580"/>
         <source>Main</source>
         <translation>Главная</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="133"/>
-        <location filename="../linux/ui_mainwindow.h" line="549"/>
+        <location filename="../mainwindow.ui" line="151"/>
+        <location filename="../linux/ui_mainwindow.h" line="571"/>
         <source>Pieces</source>
         <translation>Кол-во частей</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="140"/>
-        <location filename="../linux/ui_mainwindow.h" line="550"/>
+        <location filename="../mainwindow.ui" line="198"/>
+        <location filename="../linux/ui_mainwindow.h" line="575"/>
         <source>Creation time</source>
         <translation>Дата создания</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="147"/>
-        <location filename="../linux/ui_mainwindow.h" line="551"/>
+        <location filename="../mainwindow.ui" line="113"/>
+        <location filename="../linux/ui_mainwindow.h" line="570"/>
         <source>Created by</source>
         <translation>Создано в</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="167"/>
-        <location filename="../linux/ui_mainwindow.h" line="552"/>
+        <location filename="../mainwindow.ui" line="184"/>
+        <location filename="../linux/ui_mainwindow.h" line="573"/>
         <source>Piece size</source>
         <translation>Размер части</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="190"/>
-        <location filename="../linux/ui_mainwindow.h" line="553"/>
+        <location filename="../mainwindow.ui" line="161"/>
+        <location filename="../linux/ui_mainwindow.h" line="572"/>
         <source>Publisher</source>
         <translation>Издатель</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="197"/>
-        <location filename="../linux/ui_mainwindow.h" line="554"/>
+        <location filename="../mainwindow.ui" line="205"/>
+        <location filename="../linux/ui_mainwindow.h" line="576"/>
         <source>Name</source>
         <translation>Имя</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="204"/>
-        <location filename="../linux/ui_mainwindow.h" line="555"/>
+        <location filename="../mainwindow.ui" line="191"/>
+        <location filename="../linux/ui_mainwindow.h" line="574"/>
         <source>Url</source>
         <translation>Сайт</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="211"/>
-        <location filename="../linux/ui_mainwindow.h" line="556"/>
+        <location filename="../mainwindow.ui" line="212"/>
+        <location filename="../linux/ui_mainwindow.h" line="577"/>
         <source>Hash</source>
         <translation>Хеш</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="256"/>
-        <location filename="../linux/ui_mainwindow.h" line="557"/>
+        <location filename="../linux/ui_mainwindow.h" line="578"/>
         <source>Comment</source>
         <translation>Комментарий</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="270"/>
-        <location filename="../linux/ui_mainwindow.h" line="558"/>
+        <location filename="../linux/ui_mainwindow.h" line="579"/>
         <source>Trackers</source>
         <translation>Трекеры</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="286"/>
-        <location filename="../linux/ui_mainwindow.h" line="587"/>
+        <location filename="../linux/ui_mainwindow.h" line="608"/>
         <source>Files</source>
         <translation>Файлы</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="294"/>
-        <location filename="../linux/ui_mainwindow.h" line="561"/>
+        <location filename="../linux/ui_mainwindow.h" line="582"/>
         <source>Make Torrent</source>
         <translation>Создать торрент</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="307"/>
-        <location filename="../linux/ui_mainwindow.h" line="565"/>
+        <location filename="../linux/ui_mainwindow.h" line="586"/>
         <source>Add Files</source>
         <translation>Добавить файл</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="320"/>
-        <location filename="../linux/ui_mainwindow.h" line="569"/>
-        <location filename="../mainwindow.cpp" line="495"/>
+        <location filename="../linux/ui_mainwindow.h" line="590"/>
+        <location filename="../mainwindow.cpp" line="556"/>
         <source>Add Folder</source>
         <translation>Добавить папку</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="340"/>
-        <location filename="../linux/ui_mainwindow.h" line="573"/>
+        <location filename="../linux/ui_mainwindow.h" line="594"/>
         <source>Remove Files</source>
         <translation>Удалить файлы</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="353"/>
-        <location filename="../linux/ui_mainwindow.h" line="577"/>
+        <location filename="../linux/ui_mainwindow.h" line="598"/>
         <source>Up File</source>
         <translation>Поднять файл</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="366"/>
-        <location filename="../linux/ui_mainwindow.h" line="581"/>
+        <location filename="../linux/ui_mainwindow.h" line="602"/>
         <source>Down File</source>
         <translation>Опустить файл</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="549"/>
+        <location filename="../linux/ui_mainwindow.h" line="614"/>
+        <source>Coding</source>
+        <translation>Кодировка</translation>
     </message>
     <message>
         <source>Remove files</source>
@@ -217,144 +223,144 @@
     </message>
     <message>
         <location filename="../mainwindow.ui" line="412"/>
-        <location filename="../linux/ui_mainwindow.h" line="584"/>
+        <location filename="../linux/ui_mainwindow.h" line="605"/>
         <source>Piece size </source>
         <translation>Размер части </translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="429"/>
-        <location filename="../linux/ui_mainwindow.h" line="585"/>
+        <location filename="../linux/ui_mainwindow.h" line="606"/>
         <source>Total size </source>
         <translation>Общий размер </translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="455"/>
-        <location filename="../linux/ui_mainwindow.h" line="586"/>
+        <location filename="../linux/ui_mainwindow.h" line="607"/>
         <source>Base folder </source>
         <translation>Базовая папка </translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="468"/>
-        <location filename="../linux/ui_mainwindow.h" line="590"/>
+        <location filename="../linux/ui_mainwindow.h" line="611"/>
         <source>Raw</source>
         <translation>Исходник</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="509"/>
-        <location filename="../linux/ui_mainwindow.h" line="592"/>
+        <location filename="../linux/ui_mainwindow.h" line="613"/>
         <source>Log</source>
         <translation>Лог</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="533"/>
-        <location filename="../linux/ui_mainwindow.h" line="591"/>
+        <location filename="../linux/ui_mainwindow.h" line="612"/>
         <source>Clear</source>
         <translation>Очистить</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="119"/>
+        <location filename="../mainwindow.cpp" line="121"/>
         <source>Need to calculate piece hashes</source>
         <translation>Необходимо вычислить хеш частей</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="120"/>
+        <location filename="../mainwindow.cpp" line="122"/>
         <source>Please wait</source>
         <translation>Пожалуйста подождите</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="126"/>
+        <location filename="../mainwindow.cpp" line="128"/>
         <source>Auto</source>
         <translation>Авто</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="134"/>
+        <location filename="../mainwindow.cpp" line="136"/>
         <source>Path</source>
         <translation>Путь</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="134"/>
+        <location filename="../mainwindow.cpp" line="136"/>
         <source>Size</source>
         <translation>Размер</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="182"/>
+        <location filename="../mainwindow.cpp" line="185"/>
         <source>Create a new file</source>
         <translation>Создать новый файл</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="183"/>
+        <location filename="../mainwindow.cpp" line="186"/>
         <source>Current file is not saved. Save the file?</source>
         <translation>Текущий файл не сохранён. Сохранить файл?</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="207"/>
-        <location filename="../mainwindow.cpp" line="243"/>
+        <location filename="../mainwindow.cpp" line="210"/>
+        <location filename="../mainwindow.cpp" line="246"/>
         <source>Torrents (*.torrent)</source>
         <translation>Торренты (*.torrent)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="214"/>
+        <location filename="../mainwindow.cpp" line="217"/>
         <source>Error</source>
         <translation>Ошибка</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="214"/>
+        <location filename="../mainwindow.cpp" line="217"/>
         <source>Can&apos;t open file</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="405"/>
-        <location filename="../mainwindow.cpp" line="410"/>
+        <location filename="../mainwindow.cpp" line="465"/>
+        <location filename="../mainwindow.cpp" line="470"/>
         <source>Warning</source>
         <translation>Предупреждение</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="405"/>
+        <location filename="../mainwindow.cpp" line="465"/>
         <source>Can&apos;t make torrent for file without full path. Need to set base folder.</source>
         <translation>Невозможно создать торрент для файла без полного пути. Необходимо выбрать базовую папку.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="410"/>
+        <location filename="../mainwindow.cpp" line="470"/>
         <source>Base folder is not common.</source>
         <translation>Базовая папка не общая для всех файлов.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="473"/>
+        <location filename="../mainwindow.cpp" line="534"/>
         <source>Add File</source>
         <translation>Добавить файл</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="647"/>
+        <location filename="../mainwindow.cpp" line="711"/>
         <source>Line: %1 of %2 Col: %3</source>
         <translation>Строка: %1 из %2 Кол: %3</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="740"/>
+        <location filename="../mainwindow.cpp" line="804"/>
         <source>Error on %1 line: %2</source>
         <translation>Ошибка в строке %1: %2</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="850"/>
+        <location filename="../mainwindow.cpp" line="914"/>
         <source>B</source>
         <translation>Б</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="854"/>
+        <location filename="../mainwindow.cpp" line="918"/>
         <source>KiB</source>
         <translation>КБ</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="858"/>
+        <location filename="../mainwindow.cpp" line="922"/>
         <source>MiB</source>
         <translation>МБ</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="862"/>
+        <location filename="../mainwindow.cpp" line="926"/>
         <source>GiB</source>
         <translation>ГБ</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="866"/>
+        <location filename="../mainwindow.cpp" line="930"/>
         <source>TiB</source>
         <translation>ТБ</translation>
     </message>
