f6fdc9af18bd1534b1b66471f17de33d1a641cc0
Ivan Romanov <drizt@land.ru>
2014-11-22 02:42:23 +0500

    build full static binary on Windows

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+set -e
+
+name=torrent-file-editor
+version=beta3
+
+git clean -dfx .
+mkdir win32
+pushd win32
+mingw32-cmake .. -DCMAKE_EXE_LINKER_FLAGS=-static -DCMAKE_BUILD_TYPE=Release
+make -j5
+mv ${name}.exe ../${name}-${version}-x32.exe
+popd
+
+mkdir win64
+pushd win64
+mingw64-cmake .. -DCMAKE_EXE_LINKER_FLAGS=-static -DCMAKE_BUILD_TYPE=Release
+make -j5
+mv ${name}.exe ../${name}-${version}-x64.exe
+popd
