11d6ab7d22a55ac31c344c9903e9a8df0b8ef955
Ivan Romanov <drizt@land.ru>
2014-11-23 18:17:58 +0500

    updated translations

--- translations/torrentfileeditor_en.ts
+++ translations/torrentfileeditor_en.ts
@@ -5,19 +5,16 @@
     <name>AboutDlg</name>
     <message>
         <location filename="../aboutdlg.ui" line="14"/>
-        <location filename="../linux/ui_aboutdlg.h" line="75"/>
         <source>About Torrent File Editor</source>
         <translation>About Torrent File Editor</translation>
     </message>
     <message>
         <location filename="../aboutdlg.ui" line="27"/>
-        <location filename="../linux/ui_aboutdlg.h" line="76"/>
         <source>Torrent File Editor</source>
         <translation>Torrent File Editor</translation>
     </message>
     <message>
         <location filename="../aboutdlg.ui" line="37"/>
-        <location filename="../linux/ui_aboutdlg.h" line="77"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;
 &lt;p&gt;Author: Ivan Romanov &lt;a href=&quot;mailto:drizt@land.ru&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;drizt@land.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
 &lt;p&gt;License: &lt;/span&gt;&lt;/a&gt;GNU General Public License v3.0 or later&lt;/p&gt;
@@ -27,16 +24,6 @@
 &lt;p&gt;Author: Ivan Romanov &lt;a href=&quot;mailto:drizt@land.ru&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;drizt@land.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
 &lt;p&gt;License: &lt;/span&gt;&lt;/a&gt;GNU General Public License v3.0 or later&lt;/p&gt;
 &lt;p&gt;Home Page: &lt;a href=&quot;http://sourceforge.net/projects/torrent-file-editor/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://sourceforge.net/projects/torrent-file-editor/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
-&lt;p&gt;Sources: &lt;a href=&quot;http://github.com/drizt/torrent-file-editor&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://github.com/drizt/torrent-file-editor&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
-    </message>
-    <message>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;
-&lt;p&gt;Author: Ivan Romanov &lt;a href=&quot;mailto:drizt@land.ru&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;drizt@land.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;License: &lt;/span&gt;&lt;/a&gt;GNU General Public License v3.0 or later&lt;/p&gt;
-&lt;p&gt;Sources: &lt;a href=&quot;http://github.com/drizt/torrent-file-editor&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://github.com/drizt/torrent-file-editor&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="obsolete">&lt;html&gt;&lt;head/&gt;&lt;body&gt;
-&lt;p&gt;Author: Ivan Romanov &lt;a href=&quot;mailto:drizt@land.ru&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;drizt@land.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;License: &lt;/span&gt;&lt;/a&gt;GNU General Public License v3.0 or later&lt;/p&gt;
 &lt;p&gt;Sources: &lt;a href=&quot;http://github.com/drizt/torrent-file-editor&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://github.com/drizt/torrent-file-editor&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
@@ -70,202 +57,159 @@
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="14"/>
-        <location filename="../linux/ui_mainwindow.h" line="600"/>
         <source>MainWindow</source>
         <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="21"/>
-        <location filename="../linux/ui_mainwindow.h" line="601"/>
         <source>New</source>
         <translation>New</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="28"/>
-        <location filename="../linux/ui_mainwindow.h" line="602"/>
         <location filename="../mainwindow.cpp" line="239"/>
         <source>Open</source>
         <translation>Open</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="35"/>
-        <location filename="../linux/ui_mainwindow.h" line="603"/>
         <source>Save</source>
         <translation>Save</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="42"/>
-        <location filename="../linux/ui_mainwindow.h" line="604"/>
         <location filename="../mainwindow.cpp" line="261"/>
         <source>Save As</source>
         <translation>Save As</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="62"/>
-        <location filename="../linux/ui_mainwindow.h" line="605"/>
         <source>About</source>
         <translation>About</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="73"/>
-        <location filename="../linux/ui_mainwindow.h" line="616"/>
         <source>Main</source>
         <translation>Main</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="154"/>
-        <location filename="../linux/ui_mainwindow.h" line="607"/>
         <source>Pieces</source>
         <translation>Pieces</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="201"/>
-        <location filename="../linux/ui_mainwindow.h" line="611"/>
         <source>Creation time</source>
         <translation>Creation time</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="113"/>
-        <location filename="../linux/ui_mainwindow.h" line="606"/>
         <source>Created by</source>
         <translation>Created by</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="187"/>
-        <location filename="../linux/ui_mainwindow.h" line="609"/>
         <source>Piece size</source>
         <translation>Piece size</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="164"/>
-        <location filename="../linux/ui_mainwindow.h" line="608"/>
         <source>Publisher</source>
         <translation>Publisher</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="208"/>
-        <location filename="../linux/ui_mainwindow.h" line="612"/>
         <source>Name</source>
         <translation>Name</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="194"/>
-        <location filename="../linux/ui_mainwindow.h" line="610"/>
         <source>Url</source>
         <translation>Url</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="215"/>
-        <location filename="../linux/ui_mainwindow.h" line="613"/>
         <source>Hash</source>
         <translation>Hash</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="265"/>
-        <location filename="../linux/ui_mainwindow.h" line="614"/>
         <source>Comment</source>
         <translation>Comment</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="283"/>
-        <location filename="../linux/ui_mainwindow.h" line="615"/>
         <source>Trackers</source>
         <translation>Trackers</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="303"/>
-        <location filename="../linux/ui_mainwindow.h" line="644"/>
         <source>Files</source>
         <translation>Files</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="311"/>
-        <location filename="../linux/ui_mainwindow.h" line="618"/>
         <source>Make Torrent</source>
         <translation>Make Torrent</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="324"/>
-        <location filename="../linux/ui_mainwindow.h" line="622"/>
         <source>Add Files</source>
         <translation>Add Files</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="337"/>
-        <location filename="../linux/ui_mainwindow.h" line="626"/>
         <location filename="../mainwindow.cpp" line="573"/>
         <source>Add Folder</source>
         <translation>Add Folder</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="357"/>
-        <location filename="../linux/ui_mainwindow.h" line="630"/>
         <source>Remove Files</source>
         <translation>Remove Files</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="370"/>
-        <location filename="../linux/ui_mainwindow.h" line="634"/>
         <source>Up File</source>
         <translation>Up File</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="383"/>
-        <location filename="../linux/ui_mainwindow.h" line="638"/>
         <source>Down File</source>
         <translation>Down File</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="572"/>
-        <location filename="../linux/ui_mainwindow.h" line="650"/>
         <source>Coding</source>
         <translation>Coding</translation>
     </message>
     <message>
-        <source>Remove files</source>
-        <translation type="obsolete">Remove files</translation>
-    </message>
-    <message>
-        <source>Up file</source>
-        <translation type="obsolete">Up file</translation>
-    </message>
-    <message>
-        <source>Down file</source>
-        <translation type="obsolete">Down file</translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="432"/>
-        <location filename="../linux/ui_mainwindow.h" line="641"/>
         <source>Piece size </source>
         <translation>Piece size </translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="449"/>
-        <location filename="../linux/ui_mainwindow.h" line="642"/>
         <source>Total size </source>
         <translation>Total size </translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="478"/>
-        <location filename="../linux/ui_mainwindow.h" line="643"/>
         <source>Base folder </source>
         <translation>Base folder </translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="491"/>
-        <location filename="../linux/ui_mainwindow.h" line="647"/>
         <source>Raw</source>
         <translation>Raw</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="532"/>
-        <location filename="../linux/ui_mainwindow.h" line="649"/>
         <source>Log</source>
         <translation>Log</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="556"/>
-        <location filename="../linux/ui_mainwindow.h" line="648"/>
         <source>Clear</source>
         <translation>Clear</translation>
     </message>
--- translations/torrentfileeditor_ru.ts
+++ translations/torrentfileeditor_ru.ts
@@ -5,19 +5,16 @@
     <name>AboutDlg</name>
     <message>
         <location filename="../aboutdlg.ui" line="14"/>
-        <location filename="../linux/ui_aboutdlg.h" line="75"/>
         <source>About Torrent File Editor</source>
         <translation>О программе Torrent File Editor</translation>
     </message>
     <message>
         <location filename="../aboutdlg.ui" line="27"/>
-        <location filename="../linux/ui_aboutdlg.h" line="76"/>
         <source>Torrent File Editor</source>
         <translation>Torrent File Editor</translation>
     </message>
     <message>
         <location filename="../aboutdlg.ui" line="37"/>
-        <location filename="../linux/ui_aboutdlg.h" line="77"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;
 &lt;p&gt;Author: Ivan Romanov &lt;a href=&quot;mailto:drizt@land.ru&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;drizt@land.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
 &lt;p&gt;License: &lt;/span&gt;&lt;/a&gt;GNU General Public License v3.0 or later&lt;/p&gt;
@@ -27,16 +24,6 @@
 &lt;p&gt;Автор: Иван Романов &lt;a href=&quot;mailto:drizt@land.ru&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;drizt@land.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
 &lt;p&gt;Лицензия: &lt;/span&gt;&lt;/a&gt;GNU General Public License v3.0 or later&lt;/p&gt;
 &lt;p&gt;Домашняя страница: &lt;a href=&quot;http://sourceforge.net/projects/torrent-file-editor/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://sourceforge.net/projects/torrent-file-editor/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
-&lt;p&gt;Исхдный код: &lt;a href=&quot;http://github.com/drizt/torrent-file-editor&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://github.com/drizt/torrent-file-editor&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
-    </message>
-    <message>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;
-&lt;p&gt;Author: Ivan Romanov &lt;a href=&quot;mailto:drizt@land.ru&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;drizt@land.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;License: &lt;/span&gt;&lt;/a&gt;GNU General Public License v3.0 or later&lt;/p&gt;
-&lt;p&gt;Sources: &lt;a href=&quot;http://github.com/drizt/torrent-file-editor&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://github.com/drizt/torrent-file-editor&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="obsolete">&lt;html&gt;&lt;head/&gt;&lt;body&gt;
-&lt;p&gt;Автор: Иван Романов &lt;a href=&quot;mailto:drizt@land.ru&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;drizt@land.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;Лицензия: &lt;/span&gt;&lt;/a&gt;GNU General Public License v3.0 or later&lt;/p&gt;
 &lt;p&gt;Исходный код: &lt;a href=&quot;http://github.com/drizt/torrent-file-editor&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://github.com/drizt/torrent-file-editor&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
@@ -70,202 +57,159 @@
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="14"/>
-        <location filename="../linux/ui_mainwindow.h" line="600"/>
         <source>MainWindow</source>
         <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="21"/>
-        <location filename="../linux/ui_mainwindow.h" line="601"/>
         <source>New</source>
         <translation>Новый</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="28"/>
-        <location filename="../linux/ui_mainwindow.h" line="602"/>
         <location filename="../mainwindow.cpp" line="239"/>
         <source>Open</source>
         <translation>Открыть</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="35"/>
-        <location filename="../linux/ui_mainwindow.h" line="603"/>
         <source>Save</source>
         <translation>Сохранить</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="42"/>
-        <location filename="../linux/ui_mainwindow.h" line="604"/>
         <location filename="../mainwindow.cpp" line="261"/>
         <source>Save As</source>
         <translation>Сохранить как</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="62"/>
-        <location filename="../linux/ui_mainwindow.h" line="605"/>
         <source>About</source>
         <translation>О программе</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="73"/>
-        <location filename="../linux/ui_mainwindow.h" line="616"/>
         <source>Main</source>
         <translation>Главная</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="154"/>
-        <location filename="../linux/ui_mainwindow.h" line="607"/>
         <source>Pieces</source>
         <translation>Кол-во частей</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="201"/>
-        <location filename="../linux/ui_mainwindow.h" line="611"/>
         <source>Creation time</source>
         <translation>Дата создания</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="113"/>
-        <location filename="../linux/ui_mainwindow.h" line="606"/>
         <source>Created by</source>
         <translation>Создано в</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="187"/>
-        <location filename="../linux/ui_mainwindow.h" line="609"/>
         <source>Piece size</source>
         <translation>Размер части</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="164"/>
-        <location filename="../linux/ui_mainwindow.h" line="608"/>
         <source>Publisher</source>
         <translation>Издатель</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="208"/>
-        <location filename="../linux/ui_mainwindow.h" line="612"/>
         <source>Name</source>
         <translation>Имя</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="194"/>
-        <location filename="../linux/ui_mainwindow.h" line="610"/>
         <source>Url</source>
         <translation>Сайт</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="215"/>
-        <location filename="../linux/ui_mainwindow.h" line="613"/>
         <source>Hash</source>
         <translation>Хеш</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="265"/>
-        <location filename="../linux/ui_mainwindow.h" line="614"/>
         <source>Comment</source>
         <translation>Комментарий</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="283"/>
-        <location filename="../linux/ui_mainwindow.h" line="615"/>
         <source>Trackers</source>
         <translation>Трекеры</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="303"/>
-        <location filename="../linux/ui_mainwindow.h" line="644"/>
         <source>Files</source>
         <translation>Файлы</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="311"/>
-        <location filename="../linux/ui_mainwindow.h" line="618"/>
         <source>Make Torrent</source>
         <translation>Создать торрент</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="324"/>
-        <location filename="../linux/ui_mainwindow.h" line="622"/>
         <source>Add Files</source>
         <translation>Добавить файл</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="337"/>
-        <location filename="../linux/ui_mainwindow.h" line="626"/>
         <location filename="../mainwindow.cpp" line="573"/>
         <source>Add Folder</source>
         <translation>Добавить папку</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="357"/>
-        <location filename="../linux/ui_mainwindow.h" line="630"/>
         <source>Remove Files</source>
         <translation>Удалить файлы</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="370"/>
-        <location filename="../linux/ui_mainwindow.h" line="634"/>
         <source>Up File</source>
         <translation>Поднять файл</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="383"/>
-        <location filename="../linux/ui_mainwindow.h" line="638"/>
         <source>Down File</source>
         <translation>Опустить файл</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="572"/>
-        <location filename="../linux/ui_mainwindow.h" line="650"/>
         <source>Coding</source>
         <translation>Кодировка</translation>
     </message>
     <message>
-        <source>Remove files</source>
-        <translation type="obsolete">Удалить файлы</translation>
-    </message>
-    <message>
-        <source>Up file</source>
-        <translation type="obsolete">Поднять файл</translation>
-    </message>
-    <message>
-        <source>Down file</source>
-        <translation type="obsolete">Опустить файл</translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="432"/>
-        <location filename="../linux/ui_mainwindow.h" line="641"/>
         <source>Piece size </source>
         <translation>Размер части </translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="449"/>
-        <location filename="../linux/ui_mainwindow.h" line="642"/>
         <source>Total size </source>
         <translation>Общий размер </translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="478"/>
-        <location filename="../linux/ui_mainwindow.h" line="643"/>
         <source>Base folder </source>
         <translation>Базовая папка </translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="491"/>
-        <location filename="../linux/ui_mainwindow.h" line="647"/>
         <source>Raw</source>
         <translation>Исходник</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="532"/>
-        <location filename="../linux/ui_mainwindow.h" line="649"/>
         <source>Log</source>
         <translation>Лог</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="556"/>
-        <location filename="../linux/ui_mainwindow.h" line="648"/>
         <source>Clear</source>
         <translation>Очистить</translation>
     </message>
