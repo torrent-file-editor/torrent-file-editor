7ba1069fe56c8e58002a540c2a8ad872e09e0208
Ivan Romanov <drizt@land.ru>
2015-05-11 18:19:58 +0500

    added batch mode
    Two command line options
    --to-json - convert .torrent file to json file.
    --from-json - convert json file to .torrent file

--- main.cpp
+++ main.cpp
@@ -18,10 +18,19 @@
 
 #include "mainwindow.h"
 #include "application.h"
+#include "bencode.h"
 
+#include <QVariant>
 #include <QTranslator>
 #include <QLocale>
 #include <QFile>
+
+#ifdef HAVE_QT5
+# include <QJsonDocument>
+#else
+# include <qjson/serializer.h>
+# include <qjson/parser.h>
+#endif
 
 #ifdef HAVE_QT5
 void debugHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
@@ -38,13 +47,114 @@ void debugHandler(QtMsgType type, const char *msg)
         MainWindow::instance()->addLog(QString(msg));
 }
 
+bool toJson(const QString &source, const QString &dest)
+{
+    QFile sourceFile(source);
+    if (!sourceFile.open(QIODevice::ReadOnly)) {
+        qDebug("Error: can't open source file");
+        return false;
+    }
+
+    QByteArray raw(sourceFile.readAll());
+    sourceFile.close();
+
+    Bencode *bencode = Bencode::fromRaw(raw);
+
+    QVariant json = bencode->toJson();
+    delete bencode;
+    if (!json.isValid()) {
+        qDebug("Error: can't parse bencode format");
+        return false;
+    }
+
+#ifdef HAVE_QT5
+    QByteArray ba = QJsonDocument::fromVariant(json).toJson();
+#else
+    QJson::Serializer serializer;
+    serializer.setIndentMode(QJson::IndentFull);
+    QByteArray ba = serializer.serialize(json);
+#endif
+
+    QFile destFile(dest);
+    if (!destFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        qDebug("Error: can't open destination file");
+        return false;
+    }
+
+    destFile.write(ba);
+    destFile.close();
+    return true;
+}
+
+bool fromJson(const QString &source, const QString &dest)
+{
+    QFile sourceFile(source);
+    if (!sourceFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug("Error: can't open source file");
+        return false;
+    }
+
+    QByteArray ba(sourceFile.readAll());
+    sourceFile.close();
+
+#ifdef HAVE_QT5
+    QJsonParseError error;
+    QVariant variant = QJsonDocument::fromJson(ba, &error).toVariant();
+#else
+    QJson::Parser parser;
+    bool ok;
+    QVariant variant = parser.parse(ba, &ok);
+#endif
+
+    if (!variant.isValid()) {
+        qDebug("Error: can't parse json format");
+        return false;
+    }
+
+    Bencode *bencode = Bencode::fromJson(variant);
+
+    QFile destFile(dest);
+    if (!destFile.open(QIODevice::WriteOnly)) {
+        qDebug("Error: can't open destination file");
+        return false;
+    }
+    destFile.write(bencode->toRaw());
+    destFile.close();
+    delete bencode;
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
+    if (argc == 2 && !strcmp(argv[1], "--help")) {
+        qDebug(qPrintable("Usage: torrent-file-editor --to-json | --from-json  source dest"));
+        return 0;
+    }
+
+    if (argc == 4) {
+        QString command(argv[1]);
+        QString source(argv[2]);
+        QString dest(argv[3]);
+
+        if (command == "--to-json" || command == "--from-json") {
+            if (!QFile::exists(source)) {
+                qDebug("Error: source file is not exist!");
+                return -1;
+            }
+
+            if (command == "--to-json")
+                return toJson(source, dest) ? -1 : 0;
+            else
+                return fromJson(source, dest) ? -1 : 0;
+        }
+    }
+
 #ifdef HAVE_QT5
     qInstallMessageHandler(debugHandler);
 #else
     qInstallMsgHandler(debugHandler);
 #endif
+
     Application a(argc, argv);
 
     QString lang = QLocale().name().section('_', 0, 0);
