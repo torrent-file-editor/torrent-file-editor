e93d428a03b1163f702c9a02ac8bf9e4c8768db4
Ivan Romanov <drizt@land.ru>
2015-05-19 01:13:08 +0500

    Added Czesh translation
    Thanks to pafri
    
    fixed #4

---
+++ translations/torrentfileeditor_cs.ts
@@ -0,0 +1,357 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>B</source>
+        <translation>B</translation>
+    </message>
+    <message>
+        <source>GiB</source>
+        <translation>GiB</translation>
+    </message>
+    <message>
+        <source>KiB</source>
+        <translation>KiB</translation>
+    </message>
+    <message>
+        <source>MiB</source>
+        <translation>MiB</translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation>Nový</translation>
+    </message>
+    <message>
+        <source>Raw</source>
+        <translation>Nezpracováno</translation>
+    </message>
+    <message>
+        <source>TiB</source>
+        <translation>TiB</translation>
+    </message>
+    <message>
+        <source>Url</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Automaticky</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>Soubor</translation>
+    </message>
+    <message>
+        <source>Hash</source>
+        <translation>Hash</translation>
+    </message>
+    <message>
+        <source>Main</source>
+        <translation>Hlavní</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Název</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Otevřít</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation>Cesta</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Uložit</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Velikost</translation>
+    </message>
+    <message>
+        <source>Tree</source>
+        <translation>Strom</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>O programu</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <source>Files</source>
+        <translation>Soubory</translation>
+    </message>
+    <message>
+        <source>Make Torrent</source>
+        <translation>Udělat torrent</translation>
+    </message>
+    <message>
+        <source>Creation time</source>
+        <translation>Čas vytvoření</translation>
+    </message>
+    <message>
+        <source>Remove Files</source>
+        <translation>Odstranit soubory</translation>
+    </message>
+    <message>
+        <source>MainWindow</source>
+        <translation>Hlavní okno</translation>
+    </message>
+    <message>
+        <source>Coding</source>
+        <translation>Kódování</translation>
+    </message>
+    <message>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <source>Add Folder</source>
+        <translation>Přidat složku</translation>
+    </message>
+    <message>
+        <source>Line: %1 of %2 Col: %3</source>
+        <translation>Řádek: %1 z %2, sloupec: %3</translation>
+    </message>
+    <message>
+        <source>Torrents (*.torrent)</source>
+        <translation>Torrenty (*.torrent)</translation>
+    </message>
+    <message>
+        <source>Pieces</source>
+        <translation>Části</translation>
+    </message>
+    <message>
+        <source>Add Files</source>
+        <translation>Přidat soubory</translation>
+    </message>
+    <message>
+        <source>Open...</source>
+        <translation>Otevřít...</translation>
+    </message>
+    <message>
+        <source>Create a new file</source>
+        <translation>Vytvořit nový soubor</translation>
+    </message>
+    <message>
+        <source>Piece size </source>
+        <translation>Velikost části </translation>
+    </message>
+    <message>
+        <source>Base folder is not common.</source>
+        <translation>Základní složka není běžná.</translation>
+    </message>
+    <message>
+        <source>Save As...</source>
+        <translation>Uložit jako...</translation>
+    </message>
+    <message>
+        <source>Error on %1 line: %2</source>
+        <translation>Chyba na %1 řádku: %2</translation>
+    </message>
+    <message>
+        <source>Current file is not saved. Save the file?</source>
+        <translation>Nynější soubor není uložen. Uložit soubor?</translation>
+    </message>
+    <message>
+        <source>Trackers</source>
+        <translation>Trackery</translation>
+    </message>
+    <message>
+        <source>Can&apos;t make torrent for file without full path. Need to set base folder.</source>
+        <translation>Nelze udělat torrent pro soubor bez úplné cesty. Je potřeba nastavit základní složku.</translation>
+    </message>
+    <message>
+        <source>Created by</source>
+        <translation>Vytvořeno</translation>
+    </message>
+    <message>
+        <source>Please wait</source>
+        <translation>Počkejte, prosím</translation>
+    </message>
+    <message>
+        <source>Publisher</source>
+        <translation>Vydavatel</translation>
+    </message>
+    <message>
+        <source>About %1</source>
+        <translation>O %1</translation>
+    </message>
+    <message>
+        <source>About...</source>
+        <translation>O programu...</translation>
+    </message>
+    <message>
+        <source>Piece size</source>
+        <translation>Velikost části</translation>
+    </message>
+    <message>
+        <source>Save As</source>
+        <translation>Uložit jako</translation>
+    </message>
+    <message>
+        <source>Base folder </source>
+        <translation>Základní složka </translation>
+    </message>
+    <message>
+        <source>Comment</source>
+        <translation>Poznámka</translation>
+    </message>
+    <message>
+        <source>Add File</source>
+        <translation>Přidat soubor</translation>
+    </message>
+    <message>
+        <source>Need to calculate piece hashes</source>
+        <translation>Je potřeba spočítat hodnoty hash částí</translation>
+    </message>
+    <message>
+        <source>Remove Tree Items</source>
+        <translation>Odstranit položky stromu</translation>
+    </message>
+    <message>
+        <source>Private torrent</source>
+        <translation>Soukromý torrent</translation>
+    </message>
+    <message>
+        <source>Up File</source>
+        <translation>Posunout soubor nahoru</translation>
+    </message>
+    <message>
+        <source>Down List Item</source>
+        <translation>Posunout položku seznamu dolů</translation>
+    </message>
+    <message>
+        <source>Add Tree Item</source>
+        <translation>Přidat položku stromu</translation>
+    </message>
+    <message>
+        <source>Can&apos;t open file</source>
+        <translation>Nelze otevřít soubor</translation>
+    </message>
+    <message>
+        <source>Ctrl+Shift+S</source>
+        <translation>Ctrl+Shift+S</translation>
+    </message>
+    <message>
+        <source>uTorrent resume files(*.dat)</source>
+        <translation>Pokračovací soubory uTorrent (*.dat)</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Varování</translation>
+    </message>
+    <message>
+        <source>Down File</source>
+        <translation>Posunout soubor dolů</translation>
+    </message>
+    <message>
+        <source>Total size </source>
+        <translation>Celková velikost </translation>
+    </message>
+    <message>
+        <source>Up List Item</source>
+        <translation>Posunout položku seznamu nahoru</translation>
+    </message>
+</context>
+<context>
+    <name>BencodeModel</name>
+    <message>
+        <source>Hex</source>
+        <translation>Hex</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Název</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <source>Value</source>
+        <translation>Hodnota</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>list</source>
+        <translation>Seznam</translation>
+    </message>
+    <message>
+        <source>integer</source>
+        <translation>Celé číslo</translation>
+    </message>
+    <message>
+        <source>invalid</source>
+        <translation>Neplatný</translation>
+    </message>
+    <message>
+        <source>string</source>
+        <translation>Řetězec</translation>
+    </message>
+    <message>
+        <source>dictionary</source>
+        <translation>Slovník</translation>
+    </message>
+</context>
+<context>
+    <name>DateWidget</name>
+    <message>
+        <source>Clean</source>
+        <translation>Uklidit</translation>
+    </message>
+    <message>
+        <source>Show calendar</source>
+        <translation>Ukázat kalendář</translation>
+    </message>
+</context>
+<context>
+    <name>AboutDlg</name>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Author: Ivan Romanov &lt;a href=&quot;mailto:drizt@land.ru&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;drizt@land.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;License: GNU General Public License v3.0 or later&lt;/p&gt;&lt;p&gt;Home Page: &lt;a href=&quot;http://sourceforge.net/projects/torrent-file-editor/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://sourceforge.net/projects/torrent-file-editor/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Sources: &lt;a href=&quot;http://github.com/drizt/torrent-file-editor&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://github.com/drizt/torrent-file-editor&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Thanks to &lt;a href=&quot;http://rutracker.org/forum/profile.php?mode=viewprofile&amp;amp;u=8465051&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;ALEX Krays&lt;/span&gt;&lt;/a&gt; for application icon&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Autor: Ivan Romanov &lt;a href=&quot;mailto:drizt@land.ru&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;drizt@land.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Licence: GNU General Public License v3.0 or later&lt;/p&gt;&lt;p&gt;Stránky: &lt;a href=&quot;http://sourceforge.net/projects/torrent-file-editor/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://sourceforge.net/projects/torrent-file-editor/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Sources: &lt;a href=&quot;http://github.com/drizt/torrent-file-editor&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://github.com/drizt/torrent-file-editor&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Poděkování: &lt;a href=&quot;http://rutracker.org/forum/profile.php?mode=viewprofile&amp;amp;u=8465051&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;ALEX Krays&lt;/span&gt;&lt;/a&gt; za ikonu programu&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Torrent File Editor</source>
+        <translation>Torrent File Editor</translation>
+    </message>
+    <message>
+        <source>About Torrent File Editor</source>
+        <translation>O Torrent File Editor</translation>
+    </message>
+</context>
+<context>
+    <name>FolderEdit</name>
+    <message>
+        <source>Add Folder</source>
+        <translation>Přidat složku</translation>
+    </message>
+    <message>
+        <source>Choose folder</source>
+        <translation>Vybrat složku</translation>
+    </message>
+</context>
+<context>
+    <name>UrlEdit</name>
+    <message>
+        <source>Open in internet browser</source>
+        <translation>Otevřít v prohlížeči</translation>
+    </message>
+</context>
+</TS>
