e6cf3a3558752edc19cfd4bbc887ab465153763f
Ivan Romanov <drizt@land.ru>
2015-05-22 00:34:38 +0500

    fixed locations in Czech translation

--- translations/torrentfileeditor_cs.ts
+++ translations/torrentfileeditor_cs.ts
@@ -4,14 +4,17 @@
 <context>
     <name>AboutDlg</name>
     <message>
+        <location filename="../aboutdlg.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Author: Ivan Romanov &lt;a href=&quot;mailto:drizt@land.ru&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;drizt@land.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;License: GNU General Public License v3.0 or later&lt;/p&gt;&lt;p&gt;Home Page: &lt;a href=&quot;http://sourceforge.net/projects/torrent-file-editor/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://sourceforge.net/projects/torrent-file-editor/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Sources: &lt;a href=&quot;http://github.com/drizt/torrent-file-editor&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://github.com/drizt/torrent-file-editor&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Thanks to &lt;a href=&quot;http://rutracker.org/forum/profile.php?mode=viewprofile&amp;amp;u=8465051&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;ALEX Krays&lt;/span&gt;&lt;/a&gt; for application icon&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Autor: Ivan Romanov &lt;a href=&quot;mailto:drizt@land.ru&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;drizt@land.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Licence: GNU General Public License v3.0 or later&lt;/p&gt;&lt;p&gt;Stránky: &lt;a href=&quot;http://sourceforge.net/projects/torrent-file-editor/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://sourceforge.net/projects/torrent-file-editor/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Sources: &lt;a href=&quot;http://github.com/drizt/torrent-file-editor&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;http://github.com/drizt/torrent-file-editor&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Poděkování: &lt;a href=&quot;http://rutracker.org/forum/profile.php?mode=viewprofile&amp;amp;u=8465051&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;ALEX Krays&lt;/span&gt;&lt;/a&gt; za ikonu programu&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
+        <location filename="../aboutdlg.ui" line="72"/>
         <source>Torrent File Editor</source>
         <translation>Torrent File Editor</translation>
     </message>
     <message>
+        <location filename="../aboutdlg.ui" line="14"/>
         <source>About Torrent File Editor</source>
         <translation>O Torrent File Editor</translation>
     </message>
@@ -19,18 +22,22 @@
 <context>
     <name>BencodeModel</name>
     <message>
+        <location filename="../bencodemodel.cpp" line="655"/>
         <source>Hex</source>
         <translation>Hex</translation>
     </message>
     <message>
+        <location filename="../bencodemodel.cpp" line="653"/>
         <source>Name</source>
         <translation>Název</translation>
     </message>
     <message>
+        <location filename="../bencodemodel.cpp" line="654"/>
         <source>Type</source>
         <translation>Typ</translation>
     </message>
     <message>
+        <location filename="../bencodemodel.cpp" line="656"/>
         <source>Value</source>
         <translation>Hodnota</translation>
     </message>
@@ -38,10 +45,12 @@
 <context>
     <name>DateWidget</name>
     <message>
+        <location filename="../datewidget.cpp" line="64"/>
         <source>Clean</source>
         <translation>Uklidit</translation>
     </message>
     <message>
+        <location filename="../datewidget.cpp" line="80"/>
         <source>Show calendar</source>
         <translation>Ukázat kalendář</translation>
     </message>
@@ -49,10 +58,12 @@
 <context>
     <name>FolderEdit</name>
     <message>
+        <location filename="../folderedit.cpp" line="51"/>
         <source>Add Folder</source>
         <translation>Přidat složku</translation>
     </message>
     <message>
+        <location filename="../folderedit.cpp" line="41"/>
         <source>Choose folder</source>
         <translation>Vybrat složku</translation>
     </message>
@@ -60,266 +71,339 @@
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="../mainwindow.cpp" line="878"/>
         <source>B</source>
         <translation>B</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="890"/>
         <source>GiB</source>
         <translation>GiB</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="882"/>
         <source>KiB</source>
         <translation>KiB</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="886"/>
         <source>MiB</source>
         <translation>MiB</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="27"/>
+        <location filename="../mainwindow.ui" line="712"/>
         <source>New</source>
         <translation>Nový</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="643"/>
         <source>Raw</source>
         <translation>Nezpracováno</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="894"/>
         <source>TiB</source>
         <translation>TiB</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="213"/>
         <source>Url</source>
         <translation>URL</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="158"/>
         <source>Auto</source>
         <translation>Automaticky</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="700"/>
         <source>File</source>
         <translation>Soubor</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="234"/>
         <source>Hash</source>
         <translation>Hash</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="98"/>
         <source>Main</source>
         <translation>Hlavní</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="227"/>
         <source>Name</source>
         <translation>Název</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="34"/>
+        <location filename="../mainwindow.cpp" line="295"/>
         <source>Open</source>
         <translation>Otevřít</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="166"/>
         <source>Path</source>
         <translation>Cesta</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="41"/>
+        <location filename="../mainwindow.ui" line="728"/>
         <source>Save</source>
         <translation>Uložit</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="166"/>
         <source>Size</source>
         <translation>Velikost</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="548"/>
         <source>Tree</source>
         <translation>Strom</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="85"/>
         <source>About</source>
         <translation>O programu</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="275"/>
         <source>Error</source>
         <translation>Chyba</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="342"/>
         <source>Files</source>
         <translation>Soubory</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="353"/>
         <source>Make Torrent</source>
         <translation>Udělat torrent</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="220"/>
         <source>Creation time</source>
         <translation>Čas vytvoření</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="408"/>
         <source>Remove Files</source>
         <translation>Odstranit soubory</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="14"/>
         <source>MainWindow</source>
         <translation>Hlavní okno</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="62"/>
         <source>Coding</source>
         <translation>Kódování</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="715"/>
         <source>Ctrl+N</source>
         <translation>Ctrl+N</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="723"/>
         <source>Ctrl+O</source>
         <translation>Ctrl+O</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="731"/>
         <source>Ctrl+S</source>
         <translation>Ctrl+S</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="385"/>
+        <location filename="../mainwindow.cpp" line="549"/>
         <source>Add Folder</source>
         <translation>Přidat složku</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="690"/>
         <source>Line: %1 of %2 Col: %3</source>
         <translation>Řádek: %1 z %2, sloupec: %3</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="132"/>
         <source>Torrents (*.torrent)</source>
         <translation>Torrenty (*.torrent)</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="186"/>
         <source>Pieces</source>
         <translation>Části</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="369"/>
         <source>Add Files</source>
         <translation>Přidat soubory</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="720"/>
         <source>Open...</source>
         <translation>Otevřít...</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="247"/>
         <source>Create a new file</source>
         <translation>Vytvořit nový soubor</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="489"/>
         <source>Piece size </source>
         <translation>Velikost části </translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="466"/>
         <source>Base folder is not common.</source>
         <translation>Základní složka není běžná.</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="736"/>
         <source>Save As...</source>
         <translation>Uložit jako...</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="789"/>
+        <location filename="../mainwindow.cpp" line="797"/>
         <source>Error on %1 line: %2</source>
         <translation>Chyba na %1 řádku: %2</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="248"/>
         <source>Current file is not saved. Save the file?</source>
         <translation>Nynější soubor není uložen. Uložit soubor?</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="322"/>
         <source>Trackers</source>
         <translation>Trackery</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="461"/>
         <source>Can&apos;t make torrent for file without full path. Need to set base folder.</source>
         <translation>Nelze udělat torrent pro soubor bez úplné cesty. Je potřeba nastavit základní složku.</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="145"/>
         <source>Created by</source>
         <translation>Vytvořeno</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="152"/>
         <source>Please wait</source>
         <translation>Počkejte, prosím</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="196"/>
         <source>Publisher</source>
         <translation>Vydavatel</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="335"/>
         <source>About %1</source>
         <translation>O %1</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="744"/>
         <source>About...</source>
         <translation>O programu...</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="206"/>
         <source>Piece size</source>
         <translation>Velikost části</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="48"/>
+        <location filename="../mainwindow.cpp" line="317"/>
         <source>Save As</source>
         <translation>Uložit jako</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="535"/>
         <source>Base folder </source>
         <translation>Základní složka </translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="304"/>
         <source>Comment</source>
         <translation>Poznámka</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="526"/>
         <source>Add File</source>
         <translation>Přidat soubor</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="151"/>
         <source>Need to calculate piece hashes</source>
         <translation>Je potřeba spočítat hodnoty hash částí</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="575"/>
         <source>Remove Tree Items</source>
         <translation>Odstranit položky stromu</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="111"/>
         <source>Private torrent</source>
         <translation>Soukromý torrent</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="424"/>
         <source>Up File</source>
         <translation>Posunout soubor nahoru</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="607"/>
         <source>Down List Item</source>
         <translation>Posunout položku seznamu dolů</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="559"/>
         <source>Add Tree Item</source>
         <translation>Přidat položku stromu</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="275"/>
         <source>Can&apos;t open file</source>
         <translation>Nelze otevřít soubor</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="739"/>
         <source>Ctrl+Shift+S</source>
         <translation>Ctrl+Shift+S</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="133"/>
         <source>uTorrent resume files(*.dat)</source>
         <translation>Pokračovací soubory uTorrent (*.dat)</translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="461"/>
+        <location filename="../mainwindow.cpp" line="466"/>
         <source>Warning</source>
         <translation>Varování</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="440"/>
         <source>Down File</source>
         <translation>Posunout soubor dolů</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="506"/>
         <source>Total size </source>
         <translation>Celková velikost </translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="591"/>
         <source>Up List Item</source>
         <translation>Posunout položku seznamu nahoru</translation>
     </message>
@@ -327,22 +411,27 @@
 <context>
     <name>QObject</name>
     <message>
+        <location filename="../bencode.cpp" line="209"/>
         <source>list</source>
         <translation>Seznam</translation>
     </message>
     <message>
+        <location filename="../bencode.cpp" line="211"/>
         <source>integer</source>
         <translation>Celé číslo</translation>
     </message>
     <message>
+        <location filename="../bencode.cpp" line="213"/>
         <source>invalid</source>
         <translation>Neplatný</translation>
     </message>
     <message>
+        <location filename="../bencode.cpp" line="212"/>
         <source>string</source>
         <translation>Řetězec</translation>
     </message>
     <message>
+        <location filename="../bencode.cpp" line="210"/>
         <source>dictionary</source>
         <translation>Slovník</translation>
     </message>
@@ -350,6 +439,7 @@
 <context>
     <name>UrlEdit</name>
     <message>
+        <location filename="../urledit.cpp" line="39"/>
         <source>Open in internet browser</source>
         <translation>Otevřít v prohlížeči</translation>
     </message>
