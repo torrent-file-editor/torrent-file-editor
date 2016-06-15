02804aead203e19939bba47efe251bd0820d6017
Ivan Romanov <drizt@land.ru>
2016-06-15 15:56:41 +0500

    Improve build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 name=torrent-file-editor
 version=$(git describe --tags --dirty)
 
-git clean -dfx . -e linux
+git clean -dfx . -e linux -e linux-qt5
 mkdir win32
 pushd win32
 mingw32-cmake .. -DCMAKE_EXE_LINKER_FLAGS=-static -DCMAKE_BUILD_TYPE=Release
