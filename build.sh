daa0d2158be997f81a6925a76eaa1c2e8aa66018
Ivan Romanov <drizt@land.ru>
2016-06-24 15:21:45 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 name=torrent-file-editor
 
-git clean -dfx . -e linux -e linux-qt5
+git clean -dfx . -e linux -e linux-qt5 -e CMakeLists.txt.user
 mkdir win32
 pushd win32
 mingw32-cmake .. -DCMAKE_EXE_LINKER_FLAGS=-static -DCMAKE_BUILD_TYPE=Release
