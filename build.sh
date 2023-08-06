fa440155fc4a7eec1449b2999505e79c45234773
Ivan Romanov <drizt72@zoho.eu>
2023-08-06 12:58:12 +0500

    Fix Windows release building

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ exclude=$(echo $exclude | sed  -r 's/[^ ]+/-e &/g')
 git clean -dfx . $exclude
 mkdir win32
 pushd win32
-mingw32-cmake .. -DBUILD_SHARED=OFF -DCMAKE_EXE_LINKER_FLAGS=-static -DCMAKE_BUILD_TYPE=Release -DQT_INCLUDE_DIRS_NO_SYSTEM=ON
+mingw32-cmake .. -DQT4_BUILD=ON -DBUILD_SHARED=OFF -DCMAKE_EXE_LINKER_FLAGS=-static -DCMAKE_BUILD_TYPE=Release -DQT_INCLUDE_DIRS_NO_SYSTEM=ON
 make -j5
 version=$(cat appversion)
 i686-w64-mingw32-strip -s ${name}.exe
@@ -20,7 +20,7 @@ popd
 
 mkdir win64
 pushd win64
-mingw64-cmake .. -DBUILD_SHARED=OFF -DCMAKE_EXE_LINKER_FLAGS=-static -DCMAKE_BUILD_TYPE=Release -DQT_INCLUDE_DIRS_NO_SYSTEM=ON
+mingw64-cmake .. -DQT4_BUILD=ON -DBUILD_SHARED=OFF -DCMAKE_EXE_LINKER_FLAGS=-static -DCMAKE_BUILD_TYPE=Release -DQT_INCLUDE_DIRS_NO_SYSTEM=ON
 make -j5
 version=$(cat appversion)
 x86_64-w64-mingw32-strip -s ${name}.exe
